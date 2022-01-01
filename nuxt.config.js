295dcc3cd611665d3330aa76283a4099803000a0
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2022-01-01 16:13:26 +0200

    lazy load images

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/vue-lazysizes.server.js'
+    '~/plugins/vue-lazysizes.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
