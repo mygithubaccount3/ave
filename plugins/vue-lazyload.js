432c0b58324bc65bbf9893c786175f14d7a5ed01
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2022-01-02 21:47:47 +0200

    lazy load components

--- plugins/vue-lazyload.js
+++ plugins/vue-lazyload.js
@@ -4,5 +4,6 @@ import VueLazyload from "vue-lazyload";
 Vue.use(VueLazyload, {
   preLoad: 1.3,
   // attempt: 1,
-  loading: "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
+  loading: "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII=",
+  lazyComponent: true
 });
