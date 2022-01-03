940a064b35df7f39a272df72f9f24de2b9ccf9fb
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2022-01-03 17:48:22 +0200

    change loading image

--- plugins/vue-lazyload.js
+++ plugins/vue-lazyload.js
@@ -1,11 +1,9 @@
 import Vue from "vue";
 import VueLazyload from "vue-lazyload";
 
-const loadimage = require('../static/rotate_right_black_24dp.svg')
-
 Vue.use(VueLazyload, {
   preLoad: 1.3,
   // attempt: 1,
-  loading: loadimage,
+  loading: "/rotate_right_black_24dp.svg"
   // lazyComponent: true
 });
