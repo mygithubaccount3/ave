d26f1873ea664e382027131e41e974bb17e3fe32
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2022-01-05 15:07:22 +0200

    fix optimizing images

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
       '2xl': 1536
     },
 
-    domains: ['https://picsum.photos'],/////change
+    domains: ['picsum.photos'],/////change
     vercel: {
       baseURL: 'https://ave-nine.vercel.app/_vercel/image'
     }
