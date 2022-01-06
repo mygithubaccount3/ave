a1c5fd9c85bfe93c72bebb09fb65a1685feee9fc
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2022-01-07 00:44:58 +0200

    Revert "Revert "fix images path""
    This reverts commit 63a46879c10170267259141cb6acf6c3cc91739a.

--- components/Lookbook.vue
+++ components/Lookbook.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lookbook">
     <img
-      v-lazy="require('@/static' + imgSrc + '?webp').default" 
+      v-lazy="require('@/static' + imgSrc + '?webp').default"
       src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
       alt="lookbook image"
       width="237"
