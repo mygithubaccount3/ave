758e7fbde5d320ef047086d8092a255ad4bbcaaf
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2022-01-07 17:03:13 +0200

    Revert "Revert "fix optimizing images""
    This reverts commit 01588d7c8165a838cb407e948a568b351ab4e109.

--- server/middleware/ipx.js
+++ server/middleware/ipx.js
@@ -1,8 +1,5 @@
 import { createIPX, createIPXMiddleware } from "ipx";
-import config from "../../nuxt.config";
 
-const {
-  image: { ipx: ipxConfig }
-} = config;
-
-export default createIPXMiddleware(createIPX(ipxConfig));
+export default createIPXMiddleware(createIPX({
+    domains: ["https://picsum.photos"]
+}));
