ee67d098cc7205196e8d6f59caa2a68393ebb23c
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2022-01-07 17:32:11 +0200

    optimize immages

--- components/Lookbook.vue
+++ components/Lookbook.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lookbook">
     <img
-      :src="require(`~/assets${imgSrc}`)"
+      :src="require('~/assets' + imgSrc)"
       
       alt="lookbook image"
       width="237"
