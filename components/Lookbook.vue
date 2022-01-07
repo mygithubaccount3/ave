79470767f05634558dafba902f262347198c2f41
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2022-01-07 17:02:44 +0200

    Revert "Revert "optimize images""
    This reverts commit 3e4f6f85005fe9066c6a7f27fc824a62f6c3cc37.

--- components/Lookbook.vue
+++ components/Lookbook.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="lookbook">
     <img
-      v-lazy="require('@/static' + imgSrc + '?webp')"
-      src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
+      :src="'assets' + imgSrc + '?webp'"
+      
       alt="lookbook image"
       width="237"
       height="437"
